fcac09e80cf3ae3356246d4f9db86bf2c9069482
Sitong <nickren93@gmail.com>
2025-04-27 22:19:37 -0400

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    #pass
+    for index, dwarve in enumerate(list):
+        print(f"{index + 1}. {dwarve}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    #pass
+    return [char.capitalize() + "!" for char in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    #pass
+    for word in list:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    #pass
+    target_cheeses = ["cheddar", "gouda", "camembert"]
+    for cheese in list:
+        if cheese in target_cheeses:
+            return cheese
